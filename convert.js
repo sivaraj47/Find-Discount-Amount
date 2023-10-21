e13bfbe3c72fb885da261fbf163d7e8678ad38f2
Sivasundararaj <siva.avis47@gmail.com>
2023-10-21 16:30:00 +0530

    Create convert.js

---
+++ convert.js
@@ -0,0 +1,8 @@
+function Dsicount(){ 
+  let amt = Number(document.getElementById("inP").value);
+let percent = Number(document.getElementById("peR").value);
+let fDis = amt*(percent/100);
+let tot = fDis - amt;
+let res = document.getElementById("res");
+res.innerHTML =`Rs.${fDis}, Discount for ${percent},% <br/>Your Actual price to pay Rs.${tot} for this purchage, Keep Enjoy !`;
+  }
