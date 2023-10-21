497ea26d24d31244cf5e28daaca5fbb8889b3b04
Sivasundararaj <siva.avis47@gmail.com>
2023-10-21 16:41:55 +0530

    Update convert.js

--- convert.js
+++ convert.js
@@ -4,5 +4,5 @@ let percent = Number(document.getElementById("peR").value);
 let fDis = amt*(percent/100);
 let tot = fDis - amt;
 let res = document.getElementById("res");
-res.innerHTML =`Rs.${fDis}, Discount for ${percent}% <br/>Your Actual price to pay Rs.${tot} for this purchase Keep Enjoy !`;
+res.innerHTML =`Rs.${fDis}, Discount for ${percent}%, <br/>Your Actual price to pay Rs.${tot}, for this purchase Keep Enjoy !`;
   }
